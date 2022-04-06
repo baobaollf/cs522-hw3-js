574d8a200244bd4a70a966490d147ab90dce4c37
baobaollf <55776365+baobaollf@users.noreply.github.com>
2022-04-05 23:10:40 -0500

    updated undo&redo logic

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,7 @@ class Main extends Component {
         super(props);
         this.state = {
             inputValue: '',
-            isCentered: true,
+            pointer: 0,
             time: new Date().getTime(),
             historyStack: [],
         };
@@ -40,19 +40,25 @@ class Main extends Component {
 
     updateInputValue(evt) {
         const val = evt.target.value;
+        this.state.historyStack.splice(this.state.pointer + 1, this.state.historyStack.length);
         this.state.historyStack.push(val);
+
         this.setState({
             inputValue: val,
+            pointer: this.state.pointer + 1,
         });
     }
 
     delete() {
-        if (this.state.inputValue.length > 0 && this.state.isCentered && new Date().getTime() - this.state.time > 500) {
-            const val = this.state.inputValue.slice(0, -1);
-            this.state.historyStack.push(val);
+        if (this.state.inputValue.length > 0 && new Date().getTime() - this.state.time > 500) {
+
+            let step = 1;
+            let index = Math.max(this.state.pointer - step, 0);
+
             this.setState(
                 {
-                    inputValue: val,
+                    inputValue: this.state.historyStack[index],
+                    pointer: index,
                     time: new Date().getTime(),
                 }
             )
@@ -60,12 +66,13 @@ class Main extends Component {
     }
 
     restore() {
-        if (this.state.inputValue.length > 0 && this.state.isCentered && new Date().getTime() - this.state.time > 500) {
-            const val = this.state.historyStack[this.state.historyStack.length - 1];
-            this.state.historyStack.pop();
+        if (this.state.inputValue.length > 0 && new Date().getTime() - this.state.time > 500) {
+            let index = Math.min(this.state.pointer + 1, this.state.historyStack.length - 1);
+            console.log(this.state.historyStack)
             this.setState(
                 {
-                    inputValue: val,
+                    pointer: index,
+                    inputValue: this.state.historyStack[index],
                     time: new Date().getTime(),
                 }
             )
@@ -77,31 +84,23 @@ class Main extends Component {
     render() {
         return (
             <div>
+                <div>
+                    <Button style={{marginTop: 20}} type="primary" onClick={this.requestAccess}>Start</Button>
+                </div>
                 <TextArea rows={4} style={{marginTop: 20}} value={this.state.inputValue}
                           onChange={evt => this.updateInputValue(evt)}/>
-                {this.state.inputValue}
-                <div>
-                    <Button type="primary" onClick={this.requestAccess}>Start</Button>
-                </div>
                 <DeviceOrientation>
                     {
-                        ({absolute, alpha, beta, gamma}) => {
+                        ({alpha, beta, gamma}) => {
                             if (gamma < -45) {
-                                console.log("left");
                                 this.delete();
-                                // this.leaveCenter();
                             } else if (gamma > 45) {
-                                console.log("right")
                                 this.restore();
-                            } else {
-                                // this.atCenter();
-                                //console.log("center")
                             }
 
                             return (
                                 <div>
                                     <ul>
-                                        <li>{`Absolute: ${absolute}`}</li>
                                         <li>{`Alpha: ${alpha.toFixed(2)}`}</li>
                                         <li>{`Beta: ${beta.toFixed(2)}`}</li>
                                         <li>{`Gamma: ${gamma.toFixed(2)}`}</li>
